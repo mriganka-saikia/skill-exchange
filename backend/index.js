338dc1b5e1ce8318ec9b80e28b0d7e0eba71ba7e
mriganka-saikia <smriganka00@gmail.com>
2026-08-16 23:08:46 +0530

    chore: project setup + express server + mongodb connection

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { connectDB } = require("./config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send({ message: "Local Skill-Exchange Platform API is running" });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, async () => {
+    await connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
